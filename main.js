ed18c0d5fa2377c6db630e113bebc17546357667
Laura Comar <lautmcomar@gmail.com>
2024-10-27 13:46:16 -0400

    Handle order submission and display order summary (+added style to html)

--- main.js
+++ main.js
@@ -17,3 +17,15 @@ function updateTotalPrice() {
 //event listener for prod selection
 selectProduct.addEventListener('change', updateTotalPrice); //calls function when product changes
 inputQuantity.addEventListener('input', updateTotalPrice); // calls function when input changes
+
+// Task 4: Handle Order Submission
+const placeOrder = document.getElementById('place-order');// place order button
+const orderSummary = document.getElementById('order-summary');// display order summary
+
+placeOrder.addEventListener('click', function () {
+    const selectedProduct = selectProduct.options[selectProduct.selectedIndex].text;//get text from selected product
+    const quantity = inputQuantity.value; // get inputed quantity
+    const totalPrice = totPrice.textContent;//get tot price from display
+
+    orderSummary.textContent = `Order Details: ${quantity} ${selectedProduct}, which totals $${totalPrice}`; //order summary display
+});
